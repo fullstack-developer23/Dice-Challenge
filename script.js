75face648cc614b3347293b8e3dd488f124ef41e
fullstack-developer-23 <ahmadsafeer1997@gmail.com>
2023-11-18 20:27:58 +0000

    updated script js file

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const diceImage = document.getElementById('diceImage');
 
 let totalScore = 0;
 reset.style.display = 'none';
-score.style.display = 'block';
 button.addEventListener('click', () => {
         let diceRoll = Math.floor((Math.random() * 6) + 1);
         diceImage.src = "./images/" + diceRoll + ".gif";
@@ -17,7 +16,6 @@ button.addEventListener('click', () => {
             new Audio("./sounds/boo.mp3").play();
             totalScore = 0;
             score.textContent = totalScore;
-            score.style.display = 'block';
             button.style.display = 'none';
             reset.style.display = 'block';
         }
@@ -44,3 +42,46 @@ reset.addEventListener('click', () => {
     button.style.display = 'block';
     reset.style.display = 'none';
 });
+
+
+
+// let totalScore = 0;
+// reset.style.display = 'none';
+// score.style.display = 'block';
+// button.addEventListener('click', () => {
+//         let diceRoll = Math.floor((Math.random() * 6) + 1);
+//         diceImage.src = "./images/" + diceRoll + ".gif";
+//         new Audio("./sounds/dice.mp3").play();
+//         if(diceRoll == 1)
+//         {
+//             finalResult.textContent = "Sorry you lose. Please try again!";
+//             new Audio("./sounds/boo.mp3").play();
+//             totalScore = 0;
+//             score.textContent = totalScore;
+//             score.style.display = 'block';
+//             button.style.display = 'none';
+//             reset.style.display = 'block';
+//         }
+
+//         else{
+//             totalScore += diceRoll;
+//             score.textContent = totalScore;
+//         }
+
+//         if(totalScore >= 20)
+//         {
+//             finalResult.textContent = "Congratulations! you won.";
+//             new Audio("./sounds/congratulations.mp3").play();
+//             score.textContent = totalScore;
+//             button.style.display = 'none';
+//             reset.style.display = 'block';
+//         }
+//     });
+
+// reset.addEventListener('click', () => {
+//     finalResult.textContent = "";
+//     totalScore = 0;
+//     score.textContent = totalScore;
+//     button.style.display = 'block';
+//     reset.style.display = 'none';
+// });
